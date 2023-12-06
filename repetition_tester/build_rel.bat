a4f9e93921388294e1ef089bd64ac3be1f25b0ea
Surein Aziz <sureinaziz0@gmail.com>
2023-12-06 23:53:39 +0000

    Add debug information to repetition tester release build

--- repetition_tester/build_rel.bat
+++ repetition_tester/build_rel.bat
@@ -2,7 +2,7 @@
 
 ctime -begin repetition_tester_rel.ctm
 
-set CommonCompilerFlags=-O2 -nologo -fp:fast -fp:except- -GR- -EHa- -Oi -WX -W4 -wd4702 -wd4505 -wd4996 -wd4244 -wd4201 -wd4189 -wd4100 -FC
+set CommonCompilerFlags=-O2 -nologo -fp:fast -fp:except- -GR- -EHa- -Oi -WX -W4 -wd4702 -wd4505 -wd4996 -wd4244 -wd4201 -wd4189 -wd4100 -FC -Z7 -DDEBUG
 
 set CommonCompilerFlags= %CommonCompilerFlags% 
 set CommonLinkerFlags= -incremental:no
